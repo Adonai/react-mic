03b7e77e948fe46c13e23dea7e7b6bd8b64c45e0
Oleg Chernovskiy <adonai@xaker.ru>
2018-12-22 03:19:08 +0300

    Fix uninitialized func

--- src/libs/MicrophoneRecorder.js
+++ src/libs/MicrophoneRecorder.js
@@ -31,6 +31,33 @@ export class MicrophoneRecorder {
     mediaOptions = options;
   }
 
+  downsampleBuffer(buffer, sampleRate, outSampleRate) {
+    if (outSampleRate == sampleRate) {
+        return buffer;
+    }
+    if (outSampleRate > sampleRate) {
+        throw "downsampling rate show be smaller than original sample rate";
+    }
+    var sampleRateRatio = sampleRate / outSampleRate;
+    var newLength = Math.round(buffer.length / sampleRateRatio);
+    var result = new Int16Array(newLength);
+    var offsetResult = 0;
+    var offsetBuffer = 0;
+    while (offsetResult < result.length) {
+        var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+        var accum = 0, count = 0;
+        for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+            accum += buffer[i];
+            count++;
+        }
+
+        result[offsetResult] = Math.min(1, accum / count)*0x7FFF;
+        offsetResult++;
+        offsetBuffer = nextOffsetBuffer;
+    }
+    return result.buffer;
+  }
+
   startRecording = () => {
 
     startTime = Date.now();
@@ -116,33 +143,6 @@ export class MicrophoneRecorder {
 
       audioCtx.suspend();
     }
-  }
-
-  downsampleBuffer(buffer, sampleRate, outSampleRate) {
-    if (outSampleRate == sampleRate) {
-        return buffer;
-    }
-    if (outSampleRate > sampleRate) {
-        throw "downsampling rate show be smaller than original sample rate";
-    }
-    var sampleRateRatio = sampleRate / outSampleRate;
-    var newLength = Math.round(buffer.length / sampleRateRatio);
-    var result = new Int16Array(newLength);
-    var offsetResult = 0;
-    var offsetBuffer = 0;
-    while (offsetResult < result.length) {
-        var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
-        var accum = 0, count = 0;
-        for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
-            accum += buffer[i];
-            count++;
-        }
-
-        result[offsetResult] = Math.min(1, accum / count)*0x7FFF;
-        offsetResult++;
-        offsetBuffer = nextOffsetBuffer;
-    }
-    return result.buffer;
   }
 
   onStop(evt) {
